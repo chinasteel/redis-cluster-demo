b556f3dd29ad5c1ae7b822c6c203c30d574ef8fb
steel <gengchaogang@unisound.com>
2020-07-07 17:00:46 +0800

    add for loop test.

--- src/test/java/com/redis/cluster/demo/DemoApplicationTests.java
+++ src/test/java/com/redis/cluster/demo/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.redis.cluster.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisClusterConnection;
 import org.springframework.data.redis.connection.RedisClusterNode;
@@ -17,6 +19,7 @@ import java.util.Set;
 
 @SpringBootTest
 class DemoApplicationTests {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplicationTests.class);
     @Resource
     private RedisConnectionFactory redisConnectionFactory;
     @Resource
@@ -24,21 +27,23 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        Set<String> keys = new HashSet<>();
-        RedisClusterConnection redisClusterConnection = redisConnectionFactory.getClusterConnection();
-        Iterable<RedisClusterNode> redisClusterNodes = redisClusterConnection.clusterGetNodes();
-        redisClusterNodes.forEach(redisClusterNode -> {
-            if (!redisClusterNode.isMaster()) {
-                return;
-            }
-            ScanOptions scanOptions = ScanOptions.scanOptions().count(Integer.MAX_VALUE).match("*").build();
-            try (Cursor<byte[]> cursor = redisClusterConnection.scan(redisClusterNode, scanOptions)){
-                cursor.forEachRemaining(bytes -> keys.add(redisTemplate.getStringSerializer().deserialize(bytes)));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        keys.forEach(System.out::println);
+		for (int i = 0; i < 10000; i++) {
+			Set<String> keys = new HashSet<>();
+			RedisClusterConnection redisClusterConnection = redisConnectionFactory.getClusterConnection();
+			Iterable<RedisClusterNode> redisClusterNodes = redisClusterConnection.clusterGetNodes();
+			redisClusterNodes.forEach(redisClusterNode -> {
+				if (!redisClusterNode.isMaster()) {
+					return;
+				}
+				ScanOptions scanOptions = ScanOptions.scanOptions().count(Integer.MAX_VALUE).match("steel-*").build();
+				try (Cursor<byte[]> cursor = redisClusterConnection.scan(redisClusterNode, scanOptions)){
+					cursor.forEachRemaining(bytes -> keys.add(redisTemplate.getStringSerializer().deserialize(bytes)));
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			});
+			LOGGER.info("keys:  {}", keys);
+		}
     }
 
 }
