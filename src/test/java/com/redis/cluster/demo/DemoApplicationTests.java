3d83b8a9bb288446b5e6c1f5891fa7442e843bb9
steel <gengchaogang@unisound.com>
2020-07-07 17:24:17 +0800

    add concurrent loop test

--- src/test/java/com/redis/cluster/demo/DemoApplicationTests.java
+++ src/test/java/com/redis/cluster/demo/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.data.redis.connection.RedisClusterConnection;
 import org.springframework.data.redis.connection.RedisClusterNode;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,6 +17,8 @@ import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -24,26 +27,33 @@ class DemoApplicationTests {
     private RedisConnectionFactory redisConnectionFactory;
     @Resource
     private RedisTemplate<String, String> redisTemplate;
+	@Resource(name = "taskExecutor")
+	private AsyncTaskExecutor asyncTaskExecutor;
 
     @Test
-    void contextLoads() {
-		for (int i = 0; i < 10000; i++) {
-			Set<String> keys = new HashSet<>();
-			RedisClusterConnection redisClusterConnection = redisConnectionFactory.getClusterConnection();
-			Iterable<RedisClusterNode> redisClusterNodes = redisClusterConnection.clusterGetNodes();
-			redisClusterNodes.forEach(redisClusterNode -> {
-				if (!redisClusterNode.isMaster()) {
-					return;
-				}
-				ScanOptions scanOptions = ScanOptions.scanOptions().count(Integer.MAX_VALUE).match("steel-*").build();
-				try (Cursor<byte[]> cursor = redisClusterConnection.scan(redisClusterNode, scanOptions)){
-					cursor.forEachRemaining(bytes -> keys.add(redisTemplate.getStringSerializer().deserialize(bytes)));
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
+    void contextLoads() throws InterruptedException {
+		CountDownLatch countDownLatch = new CountDownLatch(5000);
+		for (int i = 0; i < 5000; i++) {
+			asyncTaskExecutor.submit(() -> {
+				Set<String> keys = new HashSet<>();
+				RedisClusterConnection redisClusterConnection = redisConnectionFactory.getClusterConnection();
+				Iterable<RedisClusterNode> redisClusterNodes = redisClusterConnection.clusterGetNodes();
+				redisClusterNodes.forEach(redisClusterNode -> {
+					if (!redisClusterNode.isMaster()) {
+						return;
+					}
+					ScanOptions scanOptions = ScanOptions.scanOptions().count(Integer.MAX_VALUE).match("steel-*").build();
+					try (Cursor<byte[]> cursor = redisClusterConnection.scan(redisClusterNode, scanOptions)) {
+						cursor.forEachRemaining(bytes -> keys.add(redisTemplate.getStringSerializer().deserialize(bytes)));
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				});
+				LOGGER.info("keys:  {}", keys);
+				countDownLatch.countDown();
 			});
-			LOGGER.info("keys:  {}", keys);
 		}
+		countDownLatch.await();
     }
 
 }
